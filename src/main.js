3dff7c5e221a77f6337c15763816d17bd62ba56c
chris chen <christinachen149@gmail.com>
2023-02-06 07:30:24 +0800

    git add

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(VueAxios, axios);
+
+app.mount("#app");
