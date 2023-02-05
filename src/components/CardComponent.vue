b3000fc6bae0e42aecb6b214a833053cc17ca2cd
chris chen <christinachen149@gmail.com>
2023-02-05 23:00:10 +0800

    Add Create Vue

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <div>
+    這是一個卡片元件
+    {{ text }}
+  </div>
+</template>
+
+<script src="">
+export default {
+  data() {
+    return {
+      text: '這是一段文字'
+    }
+  }
+}
+</script>
