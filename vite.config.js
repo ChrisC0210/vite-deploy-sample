fb25b654f326ed106617aa6f9cad6b18c25a42fb
chris chen <christinachen149@gmail.com>
2023-02-07 06:41:46 +0800

    add git

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import { ghPages } from "vite-plugin-gh-pages";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "/vite-deploy-sample/",
+  plugins: [vue(),ghPages()],
+  base: process.env.NODE_ENV === 'production' ? '/vite-deploy-sample/' : './',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
